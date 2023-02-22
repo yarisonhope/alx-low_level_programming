7d5f69c9840543663624c25b46bf873711198435
yarisonhope <yarison2015@gmail.com>
2023-02-22 05:43:47 -0800

    103 fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 /**
- *main - prints out first 50
+ *main - prints sum of even fibonacci
+ *suit elements
  *fibonacci suit numbers
  *Return: return 0
  */
 int main(void)
 {
-int inc;
-unsigned long n1 = 0, n2 = 1, n3;
-for (inc = 0; inc < 50; inc++)
+unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+while (n3 <= 4000000)
 {
 n3 = n1 + n2;
-printf("%lu", n3);
 n1 = n2;
 n2 = n3;
-if (inc == 49)
-printf("\n");
-else
-printf(", ");
+if ((n1 % 2) == 0)
+sum += n1;
 }
+printf("%ld\n", sum);
 return (0);
 }
