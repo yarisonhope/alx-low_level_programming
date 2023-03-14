18ea6cc589f1fd87b67d917ceabdbd684b0e690b
yarison hope <yarison2015.com>
2023-03-14 04:59:56 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,24 @@
-#include "main.h"		
-#include <stdlib.h>		
-/**		
- * create_array - create array of size size and assign char c		
- * @size: size of array	
- * @c: char to assign
- * Description: creat array of size size and assign char c		
- * Return: pointer to array, NULL if fail		
- *		
- */		
-char *create_array(unsigned int size, char c)		
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - create an array of characters
+ * @size: size of array to be created
+ * @c: character which array is to be initialized with
+ * Return: NULL is size is void; a pointer to the array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
 {
-char *str;
-unsigned int i;
-		
-		
-str = malloc(sizeof(char) * size);		
-if (size == 0 || str == NULL)
-return (NULL);
-		
-for (i = 0; i < size; i++
-str[i] = c;
-return (str);		
-}
+	char *arr;
+	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(c));
+
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
+}
--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -1,15 +1,6 @@
-#include "main.h"
-#include <unistd.h>
-		
-/**		
- * _putchar - writes the character c to stdout
- * @c: The character to print	
- *
- * Return: On success 1.		
- * On error, -1 is returned, and errno is set appropriately.		
- */		
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+int _putchar(char c);
